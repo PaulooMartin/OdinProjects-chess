905ebb342e8edd71a09cb343d255db5b0d1dd970
Paul Martin <paulmartinblopez@gmail.com>
2023-07-05 17:24:43 +0800

    Split sides of vertical&horizontal to own methods
    * For vertical, splitted it into calculating up and down separately
    * For horizontal, splitted it into calculating left and right separately

--- lib/main.rb
+++ lib/main.rb
@@ -19,33 +19,57 @@ end
 
 module MovementNonJump
   def all_moves_horizontal(coord_x, coord_y, max_moves_per_side)
-    possible_coordinates = []
-    [-1, 1].each do |shift|
-      current = coord_x
-      max_moves_per_side.times do
-        current += shift
-        possible_coordinates << [current, coord_y]
-      end
-    end
-    possible_coordinates.filter { |coords| coords[0].between?(1, 8) }
+    all_moves_left(coord_x, coord_y, max_moves_per_side) + all_moves_right(coord_x, coord_y, max_moves_per_side)
   end
 
   def all_moves_vertical(coord_x, coord_y, max_moves_per_side)
-    possible_coordinates = []
-    [-1, 1].each do |shift|
-      current = coord_y
-      max_moves_per_side.times do
-        current += shift
-        possible_coordinates << [coord_x, current]
-      end
-    end
-    possible_coordinates.filter { |coords| coords[1].between?(1, 8) }
+    all_moves_up(coord_x, coord_y, max_moves_per_side) + all_moves_down(coord_x, coord_y, max_moves_per_side)
   end
 
   def all_moves_diagonals(coord_x, coord_y, max_moves_per_side)
     diagonal_a = all_moves_upper_left(coord_x, coord_y, max_moves_per_side) + all_moves_lower_right(coord_x, coord_y, max_moves_per_side)
     diagonal_b = all_moves_lower_left(coord_x, coord_y, max_moves_per_side) + all_moves_upper_right(coord_x, coord_y, max_moves_per_side)
     diagonal_a + diagonal_b
+  end
+
+  def all_moves_left(coord_x, coord_y, max_moves)
+    possible_coordinates = []
+    current = coord_x
+    max_moves.times do
+      current -= 1
+      possible_coordinates << [current, coord_y]
+    end
+    possible_coordinates.filter { |coords| coords[0].between?(1, 8) }
+  end
+
+  def all_moves_right(coord_x, coord_y, max_moves)
+    possible_coordinates = []
+    current = coord_x
+    max_moves.times do
+      current += 1
+      possible_coordinates << [current, coord_y]
+    end
+    possible_coordinates.filter { |coords| coords[0].between?(1, 8) }
+  end
+
+  def all_moves_up(coord_x, coord_y, max_moves)
+    possible_coordinates = []
+    current = coord_y
+    max_moves.times do
+      current += 1
+      possible_coordinates << [coord_x, current]
+    end
+    possible_coordinates.filter { |coords| coords[1].between?(1, 8) }
+  end
+
+  def all_moves_down(coord_x, coord_y, max_moves)
+    possible_coordinates = []
+    current = coord_y
+    max_moves.times do
+      current -= 1
+      possible_coordinates << [coord_x, current]
+    end
+    possible_coordinates.filter { |coords| coords[1].between?(1, 8) }
   end
 
   def all_moves_upper_right(coord_x, coord_y, max_moves)
@@ -58,22 +82,22 @@ module MovementNonJump
     possible_coordinates.filter { |coords_x, coords_y| coords_x.between?(1, 8) && coords_y.between?(1, 8) }
   end
 
-  def all_moves_lower_left(coord_x, coord_y, max_moves)
-    possible_coordinates = []
-    coords = [coord_x, coord_y]
-    max_moves.times do
-      coords.map! { |coord| coord - 1 }
-      possible_coordinates << [coords[0], coords[1]]
-    end
-    possible_coordinates.filter { |coords_x, coords_y| coords_x.between?(1, 8) && coords_y.between?(1, 8) }
-  end
-
   def all_moves_upper_left(coord_x, coord_y, max_moves)
     possible_coordinates = []
     coords = [coord_x, coord_y]
     max_moves.times do
       coords[0] -= 1
       coords[1] += 1
+      possible_coordinates << [coords[0], coords[1]]
+    end
+    possible_coordinates.filter { |coords_x, coords_y| coords_x.between?(1, 8) && coords_y.between?(1, 8) }
+  end
+
+  def all_moves_lower_left(coord_x, coord_y, max_moves)
+    possible_coordinates = []
+    coords = [coord_x, coord_y]
+    max_moves.times do
+      coords.map! { |coord| coord - 1 }
       possible_coordinates << [coords[0], coords[1]]
     end
     possible_coordinates.filter { |coords_x, coords_y| coords_x.between?(1, 8) && coords_y.between?(1, 8) }

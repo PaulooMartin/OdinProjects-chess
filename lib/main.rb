47195225fdb0fd75f779bc3eeb4646e9b72b92d0
Paul Martin <paulmartinblopez@gmail.com>
2023-07-05 14:45:37 +0800

    Add getting all moves for upper/lower-right/left
    * Takes a 'maximum' that influences how many tiles the piece can move
    in said direction from a single coordinate

--- lib/main.rb
+++ lib/main.rb
@@ -41,11 +41,54 @@ module MovementNonJump
     end
     possible_coordinates.filter { |_coords_x, coords_y| coords_y.between?(1, 8) }
   end
+
+  def all_moves_upper_right(coord_x, coord_y, max_moves)
+    possible_coordinates = []
+    coords = [coord_x, coord_y]
+    max_moves.times do
+      coords.map! { |coord| coord + 1 }
+      possible_coordinates << [coords[0], coords[1]]
+    end
+    possible_coordinates.filter { |coords_x, coords_y| coords_x.between?(1, 8) && coords_y.between?(1, 8) }
+  end
+
+  def all_moves_lower_left(coord_x, coord_y, max_moves)
+    possible_coordinates = []
+    coords = [coord_x, coord_y]
+    max_moves.times do
+      coords.map! { |coord| coord - 1 }
+      possible_coordinates << [coords[0], coords[1]]
+    end
+    possible_coordinates.filter { |coords_x, coords_y| coords_x.between?(1, 8) && coords_y.between?(1, 8) }
+  end
+
+  def all_moves_upper_left(coord_x, coord_y, max_moves)
+    possible_coordinates = []
+    coords = [coord_x, coord_y]
+    max_moves.times do
+      coords[0] -= 1
+      coords[1] += 1
+      possible_coordinates << [coords[0], coords[1]]
+    end
+    possible_coordinates.filter { |coords_x, coords_y| coords_x.between?(1, 8) && coords_y.between?(1, 8) }
+  end
+
+  def all_moves_lower_right(coord_x, coord_y, max_moves)
+    possible_coordinates = []
+    coords = [coord_x, coord_y]
+    max_moves.times do
+      coords[0] += 1
+      coords[1] -= 1
+      possible_coordinates << [coords[0], coords[1]]
+    end
+    possible_coordinates.filter { |coords_x, coords_y| coords_x.between?(1, 8) && coords_y.between?(1, 8) }
+  end
 end
 
 class Rook < ChessPiece
-  include Movement
+  include MovementNonJump
 
+  # forgot to add that this was just a test
   def all_possible_moves
     coord_x, coord_y = @current_coordinates
     all_possible_moves = []

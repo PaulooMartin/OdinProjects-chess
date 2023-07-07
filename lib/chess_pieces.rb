2f2cd9465e034247b23c03b158dcf042d0a4ddb9
Paul Martin <paulmartinblopez@gmail.com>
2023-07-07 15:31:43 +0800

    Moved classes & modules in main to seperate files

---
+++ lib/chess_pieces.rb
@@ -0,0 +1,101 @@
+require_relative 'movement_non_jump'
+
+class ChessPiece
+  attr_reader :color
+  attr_accessor :current_coordinates
+
+  def initialize(color, starting_coordinates)
+    @color = color
+    @current_coordinates = starting_coordinates
+  end
+end
+
+class Pawn < ChessPiece
+  include MovementNonJump
+
+  def initialize(color, starting_coordinates)
+    super(color, starting_coordinates)
+    @moved = false
+  end
+
+  def all_possible_moves
+    coord_x, coord_y = @current_coordinates
+    max_moves = @moved ? 1 : 2
+    all_moves_up(coord_x, coord_y, max_moves)
+  end
+end
+
+class Rook < ChessPiece
+  include MovementNonJump
+
+  def all_possible_moves
+    coord_x, coord_y = @current_coordinates
+    max_moves = 7
+    all_moves_horizontal(coord_x, coord_y, max_moves) + all_moves_vertical(coord_x, coord_y, max_moves)
+  end
+end
+
+class Bishop < ChessPiece
+  include MovementNonJump
+
+  def all_possible_moves
+    coord_x, coord_y = @current_coordinates
+    max_moves = 7
+    all_moves_diagonals(coord_x, coord_y, max_moves)
+  end
+end
+
+class Horse < ChessPiece
+  def all_possible_moves
+    coord_x, coord_y = @current_coordinates
+    generate_possible_moves_horizontal(coord_x, coord_y) + generate_possible_moves_vertical(coord_x, coord_y)
+  end
+
+  private
+
+  def generate_possible_moves_horizontal(coord_x, coord_y)
+    possible_coordinates = []
+    [-2, 2].each do |shift_x|
+      [-1, 1].each do |shift_y|
+        possible_coordinates << [coord_x + shift_x, coord_y + shift_y]
+      end
+    end
+    possible_coordinates.keep_if { |x, y| x.between?(1, 8) && y.between?(1, 8) }
+  end
+
+  def generate_possible_moves_vertical(coord_x, coord_y)
+    possible_coordinates = []
+    [-1, 1].each do |shift_x|
+      [-2, 2].each do |shift_y|
+        possible_coordinates << [coord_x + shift_x, coord_y + shift_y]
+      end
+    end
+    possible_coordinates.keep_if { |x, y| x.between?(1, 8) && y.between?(1, 8) }
+  end
+end
+
+class Queen < ChessPiece
+  include MovementNonJump
+
+  def all_possible_moves
+    coord_x, coord_y = @current_coordinates
+    max_moves = 7
+    horizontal = all_moves_horizontal(coord_x, coord_y, max_moves)
+    vertical = all_moves_vertical(coord_x, coord_y, max_moves)
+    diagonal = all_moves_diagonals(coord_x, coord_y, max_moves)
+    horizontal + vertical + diagonal
+  end
+end
+
+class King < ChessPiece
+  include MovementNonJump
+
+  def all_possible_moves
+    coord_x, coord_y = @current_coordinates
+    max_moves = 1
+    horizontal = all_moves_horizontal(coord_x, coord_y, max_moves)
+    vertical = all_moves_vertical(coord_x, coord_y, max_moves)
+    diagonal = all_moves_diagonals(coord_x, coord_y, max_moves)
+    horizontal + vertical + diagonal
+  end
+end
---
+++ lib/chessboard.rb
@@ -0,0 +1,29 @@
+class Chessboard
+  def initialize
+    @board = Array.new(8) { Array.new(8, ' ') }
+  end
+
+  def print_chessboard
+    dark = 'd3d3d3'
+    light = 'white'
+    @board.reverse.each_with_index do |row, row_num|
+      bg_color = row_num.even? ? light : dark
+      print " #{8 - row_num} "
+      row.each do |piece|
+        tile = " #{piece} "
+        print Paint[tile, 'black', bg_color]
+        bg_color = switch_bg_color(bg_color)
+      end
+      puts ''
+    end
+    puts '    a  b  c  d  e  f  g  h '
+  end
+
+  private
+
+  def switch_bg_color(current)
+    dark = 'd3d3d3'
+    light = 'white'
+    current == dark ? light : dark
+  end
+end

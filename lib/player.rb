7847da769f61a4948dcb94287f2b7446230a9021
Paul Martin <paulmartinblopez@gmail.com>
2023-07-27 13:41:58 +0800

    Add #my_piece? in Player

--- lib/player.rb
+++ lib/player.rb
@@ -17,17 +17,9 @@ class Player
     player_input
   end
 
-  def move_piece(piece, destination)
-    piece.current_coordinates = destination
+  def my_piece?(chess_piece)
+    @active_pieces.include?(chess_piece)
   end
-
-  # def capture_piece(piece)
-  #   piece.owner.active_pieces.delete(piece)
-  # end
-
-  # def my_piece?(chess_piece)
-  #   chess_piece.owner == @color
-  # end
 
   private
 
